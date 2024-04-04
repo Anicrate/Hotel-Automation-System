e4c7dedb95f4ad89dd64eb79a27bcbe1794074e1
Anicrate <sahutushar29@gmail.com>
2024-04-04 10:50:56 +0530

    Final Commit

---
+++ bluebirdhotel.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2024 at 05:30 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+  CREATE Database bluebirdhotel;
+
+  USE bluebirdhotel;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bluebirdhotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp_login`
+--
+CREATE USER IF NOT EXISTS 'bluebird_user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON bluebirdhotel.* TO 'bluebird_user'@'%';
+CREATE TABLE `emp_login` (
+  `empid` int(100) NOT NULL,
+  `Emp_Email` varchar(50) NOT NULL,
+  `Emp_Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `emp_login`
+--
+
+INSERT INTO `emp_login` (`empid`, `Emp_Email`, `Emp_Password`) VALUES
+(1, 'Admin@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `NoofRoom` int(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `noofdays` int(30) NOT NULL,
+  `roomtotal` double(8,2) NOT NULL,
+  `bedtotal` double(8,2) NOT NULL,
+  `meal` varchar(30) NOT NULL,
+  `mealtotal` double(8,2) NOT NULL,
+  `finaltotal` double(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `Name`, `Email`, `RoomType`, `Bed`, `NoofRoom`, `cin`, `cout`, `noofdays`, `roomtotal`, `bedtotal`, `meal`, `mealtotal`, `finaltotal`) VALUES
+(41, 'Tushar Sahu', 'tushar@gmail.com', 'Superior Room', 'Double', 1, '2023-05-24', '2023-05-30', 6, 18000.00, 360.00, 'Half Board', 1080.00, 19440.00),
+(51, 'Ayush Shaw', 'ayushshaw127@gmail.com', 'Superior Room', 'Triple', 1, '2024-04-05', '2024-04-07', 2, 6000.00, 180.00, 'Full Board', 720.00, 6900.00),
+(52, 'Ayush Shaw', 'ayushshaw127@gmail.com', 'Superior Room', 'Quad', 1, '2024-04-26', '2024-04-30', 4, 12000.00, 480.00, 'Full Board', 1920.00, 14400.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `Email` varchar(50) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`Email`, `RoomType`, `Rating`) VALUES
+('test@gmail.com', 'Deluxe Room', 3),
+('test@gmail.com', 'Superior Room', 4),
+('test2@gmail.com', 'Single Room', 3),
+('test3@gmail.com', 'Deluxe Room', 5),
+('test4@gmail.com', 'Guest House', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(30) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `bedding` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `type`, `bedding`) VALUES
+(4, 'Superior Room', 'Single'),
+(6, 'Superior Room', 'Triple'),
+(7, 'Superior Room', 'Quad'),
+(8, 'Deluxe Room', 'Single'),
+(9, 'Deluxe Room', 'Double'),
+(10, 'Deluxe Room', 'Triple'),
+(11, 'Guest House', 'Single'),
+(12, 'Guest House', 'Double'),
+(13, 'Guest House', 'Triple'),
+(14, 'Guest House', 'Quad'),
+(16, 'Superior Room', 'Double'),
+(20, 'Single Room', 'Single'),
+(22, 'Superior Room', 'Single'),
+(23, 'Deluxe Room', 'Single'),
+(24, 'Deluxe Room', 'Triple'),
+(27, 'Guest House', 'Double'),
+(30, 'Deluxe Room', 'Single'),
+(31, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roombook`
+--
+
+CREATE TABLE `roombook` (
+  `id` int(10) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Country` varchar(30) NOT NULL,
+  `Phone` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `Meal` varchar(30) NOT NULL,
+  `NoofRoom` varchar(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `nodays` int(50) NOT NULL,
+  `stat` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `roombook`
+--
+
+INSERT INTO `roombook` (`id`, `Name`, `Email`, `Country`, `Phone`, `RoomType`, `Bed`, `Meal`, `NoofRoom`, `cin`, `cout`, `nodays`, `stat`) VALUES
+(41, 'Tushar Sahu', 'tushar@gmail.com', '', '1234567890', 'Superior Room', 'Double', 'Half Board', '1', '2023-05-24', '2023-05-30', 6, 'Confirm'),
+(51, 'Ayush Shaw', 'ayushshaw127@gmail.com', 'India', '+919330622185', 'Superior Room', 'Triple', 'Full Board', '1', '2024-04-05', '2024-04-07', 2, 'Confirm'),
+(52, 'Ayush Shaw', 'ayushshaw127@gmail.com', 'India', '+919330622185', 'Superior Room', 'Quad', 'Full Board', '1', '2024-04-26', '2024-04-30', 4, 'Confirm'),
+(53, 'Bill Gates', 'bill.gates@outlook.com', 'United States', '6685512708', 'Single Room', 'Single', 'Half Board', '1', '2024-04-30', '2024-05-16', 16, 'NotConfirm'),
+(58, 'Ayush Shaw', 'ayushshaw127@gmail.com', 'India', '1234567890', 'Guest House', 'Double', 'Breakfast', '1', '2024-04-05', '2024-04-08', 3, 'NotConfirm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE `signup` (
+  `UserID` int(100) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `signup`
+--
+
+INSERT INTO `signup` (`UserID`, `Username`, `Email`, `Password`) VALUES
+(1, 'Tushar Pankhaniya', 'tusharpankhaniya2202@gmail.com', '123'),
+(7, 'Ayush', 'ayushshaw127@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `work` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `name`, `work`) VALUES
+(14, 'Tushar Sahu', 'Manager'),
+(15, 'Ayush Shaw', 'weighter');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emp_login`
+--
+ALTER TABLE `emp_login`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roombook`
+--
+ALTER TABLE `roombook`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emp_login`
+--
+ALTER TABLE `emp_login`
+  MODIFY `empid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `roombook`
+--
+ALTER TABLE `roombook`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `signup`
+--
+ALTER TABLE `signup`
+  MODIFY `UserID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
